1924170ea5ef50f85f8a31bd42c523f399204a49
“Annah <njugunaannah@gmail.com>
2023-09-28 15:07:37 +0300

    network

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -3,6 +3,7 @@
 Finds a peak in a list of unsorted integers.
 """
 
+
 def find_peak(list_of_integers):
     """
     Finds a peak in a list of unsorted integers.
@@ -21,6 +22,7 @@ def find_peak(list_of_integers):
 
     return list_of_integers[low]
 
+
 # Testing the function
 if __name__ == "__main__":
     print(find_peak([1, 2, 4, 6, 3]))
@@ -30,4 +32,3 @@ if __name__ == "__main__":
     print(find_peak([-2, -4, 2, 1]))
     print(find_peak([4, 2, 1, 2, 2, 2, 3, 1]))
     print(find_peak([5, 4, 3, 2, 1]))  # Testing with descending order
-
