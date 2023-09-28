d1bd4e8b3323b1f10365fd13f70f3dd4ffb13774
“Annah <njugunaannah@gmail.com>
2023-09-28 15:03:04 +0300

    network

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -29,4 +29,5 @@ if __name__ == "__main__":
     print(find_peak([]))
     print(find_peak([-2, -4, 2, 1]))
     print(find_peak([4, 2, 1, 2, 2, 2, 3, 1]))
+    print(find_peak([5, 4, 3, 2, 1]))  # Testing with descending order
 
